cb2c52bc5810af8de11dea819a41b7a7a479670a
ec25736 <ec25736@qmul.ac.uk>
2025-12-02 22:47:50 +0000

    renamed files and added small tweaks

--- 1_Overview.py
+++ 1_Overview.py
@@ -2,8 +2,14 @@ import streamlit as st
 from streamlit.components.v1 import html
 from utils.map import render_ireland_map 
 
+
+st.set_page_config(page_title="Ireland Dashboard", page_icon="🇮🇪")
+
 st.title("Ireland + Northern Ireland Dashboard")
 
+"This dashboard aims to show the differing systems of 2 nations sharing an island"
+"""Some context \n
+The Republic of Ireland is a sovereign state whereas Northern Ireland is a constituent nation of the UK"""
 
 "Map:"
 map = html(render_ireland_map("data/geojson/ireland_wgs84.geojson",
